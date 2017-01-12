f4557dbfaa500c7d268c6741a5adc5d07834a724
tundish <tundish@thuswise.org>
2017-01-12 14:02:54 +0000

    Pause on packing tests.

--- tests/test_cora_transform.py
+++ tests/test_cora_transform.py
@@ -9,7 +9,7 @@ from transform.transformers.CORATransformer import CORATransformer
 
 from PyPDF2 import PdfFileReader
 
-class UKISTests(unittest.TestCase):
+class PackerTests(unittest.TestCase):
 
     """
     SDX receives:
@@ -38,18 +38,17 @@ class UKISTests(unittest.TestCase):
             ).decode("utf-8")
         )
         self.data = json.loads(
-            pkg_resources.resource_string(__name__, "replies/ukis-01.json").decode("utf-8")
+            pkg_resources.resource_string(__name__, "replies/ukis-02.json").decode("utf-8")
         )
 
     def test_ukis_base(self):
         log = logging.getLogger("test")
         tx = CORATransformer(log, self.survey, self.data)
         path = tx.create_pdf(self.survey, self.data)
-        pages = list(UKISTests.extract_text(path))
+        pages = list(PackerTests.extract_text(path))
         self.assertEqual(1, len(pages))
         self.assertIn("7.1 Any comments?", pages[0])
-        "Respondent comment data."
-        self.fail(pages)
+        self.assertIn("Respondent comment data.", pages[0])
 
     def test_ukis_zip(self):
         log = logging.getLogger("test")
@@ -57,5 +56,4 @@ class UKISTests(unittest.TestCase):
         path = tx.create_pdf(self.survey, self.data)
         images = list(tx.create_image_sequence(path, numberSeq=itertools.count()))
         index = tx.create_image_index(images)
-        self.fail(index)
         zipfile = tx.create_zip(images, index)
