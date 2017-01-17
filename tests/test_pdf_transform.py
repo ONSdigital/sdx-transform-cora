660ea5203b40c2737df943e3e69409c40dd17525
tundish <tundish@thuswise.org>
2017-01-17 11:43:45 +0000

    Substituted UKIS test case.

--- tests/test_pdf_transform.py
+++ tests/test_pdf_transform.py
@@ -6,7 +6,7 @@ from transform.transformers import PDFTransformer
 
 class TestPDFTransformer(unittest.TestCase):
     def test_localised_time(self):
-        with open("./transform/surveys/023.0203.json") as survey:
+        with open("./transform/surveys/144.xxxx.json") as survey:
             response = json.loads(test_message)
             pdf_transformer = PDFTransformer(survey, response)
 
