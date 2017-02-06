048a3dd73dde9b000b9d8d2b0f7bae09223a206a
tundish <tundish@thuswise.org>
2017-02-06 11:56:34 +0000

    Removed old PCKTransformer.

--- transform/transformers/__init__.py
+++ transform/transformers/__init__.py
@@ -2,6 +2,5 @@ from .CORATransformer import CORATransformer
 from .CSTransformer import CSTransformer
 from .PDFTransformer import PDFTransformer
 from .ImageTransformer import ImageTransformer
-from .PCKTransformer import PCKTransformer
 
-__all__ = ['CORATransformer', 'CSTransformer', 'PDFTransformer', 'ImageTransformer', 'PCKTransformer']
+__all__ = ['CORATransformer', 'CSTransformer', 'PDFTransformer', 'ImageTransformer']
