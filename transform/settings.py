db57d4b1db010a72d9bfc3be07afb76f2dce3e0d
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2017-11-16 10:29:41 +0000

    Made environment variables available as mentioned in the PR comments , removed redundant config.ini and added raising of error if environment variable not set or has a value of ""

--- transform/settings.py
+++ transform/settings.py
@@ -1,16 +1,27 @@
-import json
 import logging
 import os
+from structlog import wrap_logger
 
 LOGGING_FORMAT = "%(asctime)s|%(levelname)s: sdx-transform-cora: %(message)s"
 LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
+
+logger = wrap_logger(
+    logging.getLogger(__name__)
+)
+
+
+def _get_value(key, default_value=None):
+    """Gets a value from an environment variable , will use default if present else raise a value Error
+    """
+    value = os.getenv(key, default_value)
+    if not value:
+        logger.error("No value set for {}".format(key))
+        raise ValueError()
+    return value
 
 SDX_FTP_IMAGES_PATH = "EDC_QImages"
 SDX_FTP_DATA_PATH = "EDC_QData"
 SDX_FTP_RECEIPT_PATH = "EDC_QReceipts"
 
-
-if not os.getenv("CF_DEPLOYMENT", False):
-    SDX_SEQUENCE_URL = os.getenv("SDX_SEQUENCE_URL", "")
-    FTP_PATH = os.getenv("FTP_PATH", "\\\\NP3RVWAPXX370\\SDX_preprod\\")
-
+SDX_SEQUENCE_URL = _get_value("SDX_SEQUENCE_URL", "http://sdx-sequence:5000")
+FTP_PATH = _get_value("FTP_PATH", "\\\\NP3RVWAPXX370\\SDX_preprod\\")
